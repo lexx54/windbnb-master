bb757b1cb7b07535e79ddec9955bd038410d15f2
lexx54 <chapelexx54@gmail.com>
2020-12-29 22:05:52 -0400

    change function use in get values for a fetch

--- getData.js
+++ getData.js
@@ -1,25 +1,31 @@
+
 export async function getValues(){
-  const myData=await promiseMaker("http://www.json-generator.com/api/json/get/coXCVpXYrS?indent=2");
+  const myData=await fetch("./stays.json").then(ele=>ele.json());
+  // const dataJson=await myData.then(ele=>ele.json())
   return myData;
 }
+// export async function getValues(){
+//   const myData=await promiseMaker("https://github.com/lexx54/windbnb-master/blob/master/stays.json");
+//   return myData;
+// }
   
-function promiseMaker(url) {
-  return new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest();
-    req.responseType = "json";
-    req.open("get", url);
+// function promiseMaker(url) {
+//   return new Promise((resolve, reject) => {
+//     const req = new XMLHttpRequest();
+//     req.responseType = "json";
+//     req.open("get", url);
 
-    req.onload = function () {
-      try {
-        if (this.status === 200) resolve(this.response);
-        else reject("the response wasn't correct")
-      } catch {
-        console.error("dont work")
-      }
-    }
-    req.onerror = () => {
-      reject(new Error("somenthing went wrong when reciving response"))
-    }
-    req.send()
-  })
-}
+//     req.onload = function () {
+//       try {
+//         if (this.status === 200) resolve(this.response);
+//         else reject("the response wasn't correct")
+//       } catch {
+//         console.error("dont work")
+//       }
+//     }
+//     req.onerror = () => {
+//       reject(new Error("somenthing went wrong when reciving response"))
+//     }
+//     req.send()
+//   })
+// }
